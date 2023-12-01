44c2f5bdf48472d83e24236db2d32950f58b2cb7
sebmagia <senunez@udec.cl>
2023-12-01 17:38:34 +0100

    Set up EAFZ zone

---
+++ Code/make_initial_files.py
@@ -0,0 +1,106 @@
+import yaml
+import numpy as np
+from obspy.clients.fdsn import Client
+from obspy.core import UTCDateTime
+import pathlib
+from utils import load_config
+
+
+def setup_region(client: Client, config: dict, t0: UTCDateTime, tf: UTCDateTime):
+    """Set up region based on configuration."""
+    return client.get_stations(
+        starttime=t0,
+        endtime=tf,
+        network=config['network'],
+        station='*',
+        minlatitude=config['latitude_range'][0],
+        maxlatitude=config['latitude_range'][1],
+        minlongitude=config['longitude_range'][0],
+        maxlongitude=config['longitude_range'][1]
+    )[0]
+
+def extract_station_data(stations):
+    """Extract station code, location and elevation from the stations list."""
+    stas = [station.code for station in stations]
+    locs = [np.array([station.latitude, station.longitude, station.elevation]).reshape(1, -1) for station in stations]
+
+    stas = np.hstack(stas)
+    locs = np.vstack(locs)
+
+    iarg = np.argsort(locs[:, 0])
+    return locs[iarg], stas[iarg]
+
+def save_files(base_path: str, locs, stas, config, years):
+    """Save network, region, and 1D velocity model files."""
+    # Save network file
+    np.savez_compressed(base_path + 'stations.npz', locs=locs, stas=stas)
+
+    # Save region file
+    np.savez_compressed(
+        base_path + 'region.npz',
+        lat_range=config['latitude_range'],
+        lon_range=config['longitude_range'],
+        depth_range=config['depth_range'],
+        deg_pad=config['degree_padding'],
+        num_grids=config['number_of_grids'],
+        n_spatial_nodes=config['number_of_spatial_nodes'],
+        years=years,
+        load_initial_files=config['load_initial_files'],
+        use_pretrained_model=config['use_pretrained_model']
+    )
+
+    # Save 1D velocity model
+    np.savez_compressed(
+        base_path + '1d_velocity_model.npz',
+        Depths=np.array(config['velocity_model']['Depths']),
+        Vp=np.array(config['velocity_model']['Vp']),
+        Vs=np.array(config['velocity_model']['Vs'])
+    )
+def save_files_nosta(base_path: str, config, years):
+    """Save network, region, and 1D velocity model files."""
+    # Save network file
+
+
+    # Save region file
+    np.savez_compressed(
+        base_path + 'region.npz',
+        lat_range=config['latitude_range'],
+        lon_range=config['longitude_range'],
+        depth_range=config['depth_range'],
+        deg_pad=config['degree_padding'],
+        num_grids=config['number_of_grids'],
+        n_spatial_nodes=config['number_of_spatial_nodes'],
+        years=years,
+        load_initial_files=config['load_initial_files'],
+        use_pretrained_model=config['use_pretrained_model']
+    )
+
+    # Save 1D velocity model
+    np.savez_compressed(
+        base_path + '1d_velocity_model.npz',
+        Depths=np.array(config['velocity_model']['Depths']),
+        Vp=np.array(config['velocity_model']['Vp']),
+        Vs=np.array(config['velocity_model']['Vs'])
+    )
+
+
+if __name__ == '__main__':
+    print("Loading configuration from 'config.yaml'...")
+    config = load_config('config.yaml')
+
+    print("Setting up time range...")
+    t0 = UTCDateTime(config['time_range']['start'])
+    tf = UTCDateTime(config['time_range']['end'])
+    years = list(range(t0.year, tf.year + 1))
+
+   
+
+
+    print("Determining base path for saving files...")
+    base_path = str(pathlib.Path().absolute()) + ('\\' if '\\' in str(pathlib.Path().absolute()) else '/')
+    
+    print("Saving files...")
+    save_files_nosta(base_path, config, years)
+
+    print("All files saved successfully!")
+    print("✔ Script execution: Done")
---
+++ README.md
@@ -0,0 +1,26 @@
+# GENIE : Graph Earthquake Neural Interpretation Engine
+
+A Graph Neural Network (GNN) based earthquake phase associator and spatio-temporal source localization model. 
+
+The paper associated with this work is given at https://pubs.geoscienceworld.org/ssa/bssa/article/doi/10.1785/0120220182/619845/Earthquake-Phase-Association-with-Graph-Neural.
+
+The source code is posted at https://github.com/imcbrearty/GENIE/tree/main/Code.
+
+Note that this is an early release of the code. Increased documentation, more thorough user testing, and pre-trained models will be forthcoming.
+
+## Applying the model
+
+For now, the basic workflow is to (i). install dependencies in "install_dependencies.txt", (ii). run the "make_initial_files.py" script to initialize station, region, and velocity model files, (iii). run "assemble_network_data.py" for an input set of stations and spatial region, which sets up the directory and initilizes required variables, (iv). run "calculate_travel_times.py" to compute the travel time grid of P and S phases over the region of interest, for a chosen velocity model, (v). run "train_GENIE_model.py" to train the GNN for the given application, and (vi). run "process_continuous_days.py" to compute predictions and build an earthquake catalog for a given set of input picks, and the current trained GNN and velocity model.
+
+The config files can be used to adjust most parameters. Also, station, region, and 1D velocity model files can be created following the same format as shown in make_initial_files.py without actually having to run make_initial_files.py.
+
+Pre-trained GNN's, and pre-computed travel time fields will be supplied in the future, to faciliate easier use and allow users to only have to run steps (i-iii), and (vi). above. Additionally, the "process_continuous_days.py" script will be updated soon to improve the way the GNN is applied over continuous days and the resulting catalog that is built.
+
+
+## Extra information
+
+The pre-print is given at https://arxiv.org/abs/2209.07086. The datasets used in this study are available in https://github.com/imcbrearty/GENIE/tree/main/BSSA.
+
+For a description of a related GNN architecture applied to source localization from discrete pick datasets, see https://ieeexplore.ieee.org/document/9897468, https://arxiv.org/abs/2203.05144.
+
+Preliminary results of the method are shown here, https://www.scec.org/meetings/2021/am/poster/227.
